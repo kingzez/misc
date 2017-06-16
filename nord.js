1c07185ba3af76741441d35089b27f20bf4e0fa5
illusaen <tilphousia@gmail.com>
2017-06-16 09:55:57 -0400

    Add files via upload

---
+++ nord.js
@@ -0,0 +1,4 @@
+t.prefs_.set('color-palette-overrides',["#2E3440", "#BF616A", "#A3BE8C", "#EBCB8B", "#88C0D0", "#B48EAD", "#8FBCBB", "#4C566A", "#434C5E", "#B48EAD", "#3B4252", "#434C5E", "#4C566A", "#D8DEE9", "#E5E9F0", "#ECEFF4"]);
+t.prefs_.set('foreground-color', "#D8DEE9");
+t.prefs_.set('background-color', "#2E3440");
+t.prefs_.set('cursor-color', 'rgba(165,162,162,0.5)');
